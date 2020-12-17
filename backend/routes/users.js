c38509bbea7343f09c7ed5d360c90a2b67d450ff
Nikita Ustimenko <ustimencko.nickita@yandex.ru>
2020-12-17 18:06:29 +0300

    fix: Исправляю авторизацию

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -39,9 +39,6 @@ router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    name: Joi.string().required().min(2).max(30),
-    avatar: Joi.string(),
-    about: Joi.string().min(2).max(30),
   }),
 }), login); // роут для логина
 
@@ -49,6 +46,9 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+    about: Joi.string().min(2).max(30),
   }),
 }), createUser); // создаёт пользователя
 
